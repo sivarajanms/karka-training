7bf355e2119fdd7ddf711d5ab86316b147201205
sivarajanms <sivarajanms14@gmail.com>
2023-07-13 12:44:13 +0530

    python practice

--- D11-20230707/assignment/problem15.py
+++ D11-20230707/assignment/problem15.py
@@ -1,11 +1,17 @@
-gender=input("what is your gender(M or F):")
+gender=input("what is your gender(m or f):")
 f_name=input("enter your firstname:")
 l_name=input("enter your lastname:")
-age=int(input('age'))
-married=input(f"are you married.{f_name},(y or n)?")
-
-
-def check(age):
-    if(age<=20):
-        print(f"then i shall call you mrs.{f_name}")
-        
+age=int(input('age:'))
+if(gender=='m') and (age>20):
+    print(f"then i shall call you mr.{f_name}{l_name}.")
+elif(gender=='m') and (age<20):
+    print(f"then i shall call you {f_name}{l_name}.")    
+elif(gender=='f') and (age>20):
+    married=input("are you married.(y or n)?") 
+    if(married=='y'):
+        print(f"then i shall call you mrs{f_name}{l_name}.")         
+    elif(married=='n'):    
+        print(f"then i shall call you ms{f_name}{l_name}.")
+elif(gender=='f') and (age<20):
+    print(f"then i shall call you ms{f_name}{l_name}.")
+   
